1afc35c4922a27588837b58a8894169070389641
BaRoNeaS <41582600+BaRoNeaS@users.noreply.github.com>
2018-11-11 16:02:35 +0300

    Update say.js

--- cmds/say.js
+++ cmds/say.js
@@ -1,9 +1,13 @@
 module.exports.run = async (bot, message, args) => {
     
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
-
+	let mesaj = args.slice(0).join(' ');
+	if (mesaj.length < 1) return message.reply('Yazmam İçin Birşey Yazmalısın!');
+    message.delete();
+    const embed = new Discord.RichEmbed()
+    .setAuthor('')
+    .setColor(3447003)
+    .setDescription(`${mesaj}`)
+    return message.channel.sendEmbed(embed);
 }
 
 module.exports.help = {
