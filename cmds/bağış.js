6bc599c85be005ac3242d92bcc814c6a7f385955
BaRoNeaS <41582600+BaRoNeaS@users.noreply.github.com>
2018-11-12 17:48:21 +0300

    Update and rename buy.js to bağış.js

---
+++ cmds/bağış.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    let inline = true
+    let bicon = bot.user.displayAvatarURL;
+ 
+    let embed = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .setTitle("Donate Bot")
+    .setThumbnail(bicon)
+    .addField("You can order your own discord bot from", "<@350261026031271938>")
+    .addField("Bot Komutları", "Özel komutlar", inline)
+    .addField("Xp Sistemi", "İsteğe Bağlı", inline)
+    .addField("Özel Bot", "Hyr,Hekez Davet Edebilir")
+    .addField("Bağış Linki","[Bağış Linki](https://www.bynogame.com/destekle/genceryunuskurt)")
+    .addField("Yardım", "Herhangi bir sorunuz varsa Destek Sunucusunda sormaktan çekinmeyin. [Yardım Sunucusu](https://discord.gg/Kxj4edY)")
+
+    message.channel.send(embed)
+
+}
+    module.exports.help = {
+        name: "bağış"
+}
